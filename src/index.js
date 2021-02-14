bedd9341043990776fc5a930ff77e56e079bd517
Jun-cloud <kjy8827@gmail.com>
2021-02-14 10:30:23 +0900

    useState part one

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './App.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  // Hooks는 React의 state machine에 연결하는 기본적인 방법
+  // 변수명의 경우 꼭 item, setItem과 같은 형식일 필요는 없음
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>React Hooks useState { item }</h1>
+      <button onClick={ incrementItem }>Increment</button>
+      <button onClick={ decrementItem }>Decrement</button>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement)
