50ff84bc10d7ab1c467fb656c4ac5c79ef3f6d11
Jun-cloud <kjy8827@gmail.com>
2021-02-16 21:37:01 +0900

    useInput part one

--- src/index.js
+++ src/index.js
@@ -2,55 +2,23 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-// React hooks
-const App = () => {
-  // Hooks는 React의 state machine에 연결하는 기본적인 방법
-  // 변수명의 경우 꼭 item, setItem과 같은 형식일 필요는 없음
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
-  return (
-    <div className="App">
-      <h1>React Hooks useState { item }</h1>
-      <button onClick={ incrementItem }>Increment</button>
-      <button onClick={ decrementItem }>Decrement</button>
-    </div>
-  );
+const useInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = event => {
+        console.log(event.target);
+    }
+    return { value, onChange };
 }
 
-// 기존 방식 - 기존 방식과 hooks를 비교하면 코드의 양부터 차이가 많이남
-class ClassApp extends React.Component {
-  state = {
-    item: 1
-  }
-
-  render() {
-    const { item } = this.state;
+const App = () => {
+    const name = useInput("Name is ");
     return (
-      <div className="App">
-      <h1>React Hooks useState { item }</h1>
-      <button onClick={ this.incrementItem }>Increment</button>
-      <button onClick={ this.decrementItem }>Decrement</button>
+    <div className="App">
+        <h1>React Hooks useInput</h1>
+        <input placeholder="Name" { ...name } />
     </div>
-    )
-  }
-
-  incrementItem = () => {
-    this.setState(state => {
-      return {
-        item: state.item + 1
-      }
-    })
-  }
-
-  decrementItem = () => {
-    this.setState(state => {
-      return {
-        item: state.item - 1
-      }
-    })
-  }
+    );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<ClassApp />, rootElement)
+ReactDOM.render(<App />, rootElement)
