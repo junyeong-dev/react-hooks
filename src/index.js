578b9f0b9e61113e10003fbb61abb63b2a559f76
Jun-cloud <kjy8827@gmail.com>
2021-02-20 17:59:29 +0900

    useTitle

--- src/index.js
+++ src/index.js
@@ -2,26 +2,25 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-const App = () => {
-    // useEffect는 ComponentDidmount, ComponentWillUnMount, ComponentDidUpdate의 역할을 함
-    const hello = () => console.log("hello");
-    const [numberOne, setNumberOne] = useState(0);
-    const [numberTwo, setNumberTwo] = useState(0);
-    /*
-      useEffect의 첫번째 매개변수는 함수이고 두번째 매개변수는 dependency
-      useEffect(hello) : 항상 hello 함수가 실행됨
-      useEffect(hello, [numberOne]) : numberOne의 값이 변할 때 실행됨
-      useEffect(hello, []) : 딱 한번만 실행됨(로드될 때)
-    */
-    useEffect(hello, []);
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  }
+  useEffect(updateTitle, [title]);
+  return setTitle;
+}
 
-    return (
-    <div className="App">
-        <h1>React Hooks useTabs</h1>
-        <button onClick={ () => setNumberOne(numberOne + 1) }>{ numberOne }</button>
-        <button onClick={ () => setNumberTwo(numberTwo + 1) }>{ numberTwo }</button>
-    </div>
-    );
+const App = () => {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+
+  return (
+  <div className="App">
+      <h1>React Hooks useTitle</h1>
+  </div>  
+  );
 }
 
 const rootElement = document.getElementById("root");
---
+++ src/useEffect.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+const App = () => {
+    // useEffect는 ComponentDidmount, ComponentWillUnMount, ComponentDidUpdate의 역할을 함
+    const hello = () => console.log("hello");
+    const [numberOne, setNumberOne] = useState(0);
+    const [numberTwo, setNumberTwo] = useState(0);
+    /*
+      useEffect의 첫번째 매개변수는 함수이고 두번째 매개변수는 dependency
+      useEffect(hello) : 항상 hello 함수가 실행됨
+      useEffect(hello, [numberOne]) : numberOne의 값이 변할 때 실행됨
+      useEffect(hello, []) : 딱 한번만 실행됨(로드될 때)
+    */
+    useEffect(hello, []);
+
+    return (
+    <div className="App">
+        <h1>React Hooks useEffect</h1>
+        <button onClick={ () => setNumberOne(numberOne + 1) }>{ numberOne }</button>
+        <button onClick={ () => setNumberTwo(numberTwo + 1) }>{ numberTwo }</button>
+    </div>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement)
